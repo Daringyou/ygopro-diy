e044c6dd1361412c2eec4e9558eb411642605a2f
Daringyou <1955313469@qq.com>
2024-11-18 22:54:46 +0800

    modified:   script/c10911034.lua

--- script/c10911034.lua
+++ script/c10911034.lua
@@ -142,18 +142,14 @@ function s.PrivateEffect(c)
 	local e1 = Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id, 1))
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CLIENT_HINT)
-	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE, 0)
+	e1:SetTarget(s.eftg)
 	e1:SetValue(s.efilter1)
 	e1:SetLabel(1)
-	local ge1 = Effect.CreateEffect(c)
-	ge1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_GRANT)
-	ge1:SetRange(LOCATION_MZONE)
-	ge1:SetTargetRange(LOCATION_MZONE, 0)
-	ge1:SetTarget(s.granttg)
-	ge1:SetLabelObject(e1)
-	c:RegisterEffect(ge1)
+	c:RegisterEffect(e1)
 	--卡组抽卡
 	local e2 = Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id, 2))
@@ -247,7 +243,7 @@ function s.addval(e, c)
 	return c:GetFlagEffect(aux.SkyCode) > 0
 end
 
-function s.granttg(e, c)
+function s.eftg(e, c)
 	return aux.SkyCodePlayer[e:GetHandlerPlayer()] >= e:GetLabelObject():GetLabel() and c:GetFlagEffect(aux.SkyCode) > 0
 end
 
